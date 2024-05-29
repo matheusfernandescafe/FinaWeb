3a8509a5b994f6d1f341fbf4571b0bb231e5f213
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2024-05-28 22:25:44 -0300

    feat: Criado Handler de Transaction

---
+++ Fina.Core/Handler/ITransactionHandler.cs
@@ -0,0 +1,15 @@
+﻿using Fina.Core.Models;
+using Fina.Core.Requests.Transactions;
+using Fina.Core.Responses;
+
+namespace Fina.Core.Handler;
+
+public interface ITransactionHandler : IBaseHandler<
+    CreateTransactionRequest,
+    UpdateTransactionRequest,
+    DeleteTransactionRequest,
+    GetTransactionByIdRequest,
+    Transaction>
+{
+    Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionByPeriodRequest request);
+}
