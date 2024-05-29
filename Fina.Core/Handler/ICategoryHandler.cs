6f78fa603b3e132d758bb1f80552bb6aa7cc1fd9
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2024-05-28 22:25:36 -0300

    feat: Criado Handler de Categoria

---
+++ Fina.Core/Handler/ICategoryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Fina.Core.Models;
+using Fina.Core.Requests.Categories;
+using Fina.Core.Responses;
+
+namespace Fina.Core.Handler;
+
+public interface ICategoryHandler : IBaseHandler<
+    CreateCategoryRequest,
+    UpdateCategoryRequest,
+    DeleteCategoryRequest,
+    GetCategoryByIdRequest,
+    Category>
+{
+    Task<PagedResponse<List<Category>?>> GetAllAsync(GetAllCategoryRequest request);
+}
