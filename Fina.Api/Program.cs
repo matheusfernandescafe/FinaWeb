ad574aacb32ef323ca980162962e68f6fe73b216
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2024-05-31 16:28:33 -0300

    feat: Configurado conexão com o banco, adicionado Mappings e realizado primeira Migrations

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,4 +1,21 @@
+using Fina.Api.Data;
+using Fina.Api.Handlers;
+using Fina.Core.Handlers;
+using Fina.Core.Requests.Categories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string connectionString =
+    "Server=(localdb)\\mssqllocaldb;Database=fina;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+builder.Services.AddDbContext<AppDbContext>(
+    x => x.UseSqlServer(connectionString)
+);
+
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
