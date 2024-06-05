951213596aedb133830381dc98ad69c67d53b09d
Matheus Fernandes <42787903+cafepng@users.noreply.github.com>
2024-06-05 00:19:45 -0300

    feat(Pages): Adição das páginas para "Create" e "GetAll"

---
+++ Fina.Web/Pages/Categories/GetAll.razor.cs
@@ -0,0 +1,92 @@
+﻿using Fina.Core.Handlers;
+using Fina.Core.Models;
+using Fina.Core.Requests.Categories;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Fina.Web.Pages.Categories;
+
+public partial class GetAllCategoriesPage : ComponentBase
+{
+    #region Properties
+
+    public bool IsBusy { get; set; } = false;
+    public List<Category> Categories { get; set; } = [];
+
+    #endregion
+
+
+    #region Services
+
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    [Inject]
+    public IDialogService Dialog { get; set; } = null!;
+
+    [Inject]
+    public ICategoryHandler Handler { get; set; } = null!;
+
+    #endregion
+
+
+    #region Overrides
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsBusy = true;
+        try
+        {
+            var request = new GetAllCategoriesRequest();
+            var result = await Handler.GetAllAsync(request);
+            if (result.IsSuccess)
+                Categories = result.Data ?? [];
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    #endregion
+
+    #region Functions
+
+    public async void OnDeleteButtonClickedAsync(Category category)
+    {
+        var result = await Dialog.ShowMessageBox(
+            "ATENÇÃO",
+            $"Ao prosseguir a categoria '{category.Title}' será removida. Deseja continuar?",
+            yesText: "Excluir",
+            cancelText: "Cancelar");
+
+        if (result is true)
+            await OnDeleteAsync(category);
+
+        StateHasChanged();
+    }
+
+    public async Task OnDeleteAsync(Category category)
+    {
+        try
+        {
+            var request = new DeleteCategoryRequest
+            {
+                Id = category.Id
+            };
+            await Handler.DeleteAsync(request);
+            Categories.RemoveAll(x => x.Id == category.Id);
+            Snackbar.Add($"Categoria '{category.Title}' removida", Severity.Info);
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+    }
+
+    #endregion
+}
